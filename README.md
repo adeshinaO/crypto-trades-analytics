49131c302352384c8ca89e2ce097ccfca3c96a50
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2021-01-23 02:58:55 +0100

    added GIF demo

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # BTC/USD Analytics
 
 A data pipeline that collects data for trades of BTC/USD executed on 
-[exchange.blockchain.com](https://exchange.blockchain.com) and feeds a dashboard showing the quanity of
-BTC/USD sold/bought per minute.
+[exchange.blockchain.com](https://exchange.blockchain.com) and feeds a dashboard showing the quantity of
+BTC/USD traded every minute. The charts are updated in near real-time.
 
-TODO: GIF of dashboard.
+![demo](crypto-demo.gif)
 
-## Pipeline Architecture
+## Pipeline Architecture Overview
 
-![](crypto-anly.png)
+![architecture-diagram](crypto-anly.png)
 
 The first component of the pipeline is `Extractor`, A command line Java program written with JDK 11. It opens a 
 persistent WebSocket connection to the [Blockchain WebSocket API](https://exchange.blockchain.com/api/#websocket-api). 
@@ -20,7 +20,10 @@ polls the Kafka `input topic`, aggregates the data and writes to the `output top
 The final component of the pipeline is a Spring Boot application that serves the frontend assets for the web UI, and 
 a provides a REST endpoint for fetching data that is displayed on charts. The UI consists of bar charts drawn using 
 the ChartJS library. The frontend calls the data endpoint every 10 seconds and this causes the application to poll 
-the Kafka `output topic`, package and return any available data.
+the Kafka `output topic`.
+
+There is no database. A crude Key-Value store is implemented in memory using `HashMap`s. The contents of this datastore
+are sent in response to API calls from the frontend, and it is updated if polling Kafka returns any items.
 
 ## Tech Stack
 
--- crypto-analytics-dashboard/src/main/java/io/github/abdulwahabo/cryptoanalytics/dashboard/service/impl/DashboardServiceImpl.java
+++ crypto-analytics-dashboard/src/main/java/io/github/abdulwahabo/cryptoanalytics/dashboard/service/impl/DashboardServiceImpl.java
@@ -4,9 +4,13 @@ import io.github.abdulwahabo.cryptoanalytics.common.model.AggregateTradeData;
 import io.github.abdulwahabo.cryptoanalytics.dashboard.model.DashboardDataset;
 import io.github.abdulwahabo.cryptoanalytics.dashboard.service.DashboardService;
 import io.github.abdulwahabo.cryptoanalytics.dashboard.service.KafkaConsumerService;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,10 @@ import org.springframework.stereotype.Service;
 public class DashboardServiceImpl implements DashboardService {
 
     private KafkaConsumerService kafkaConsumerService;
+
+    // Crude datastore.
+    private Map<String, DashboardDataset.Data> buyDataCache = new HashMap<>(60);
+    private Map<String, DashboardDataset.Data> sellDataCache = new HashMap<>(60);
 
     @Autowired
     public DashboardServiceImpl(KafkaConsumerService kafkaConsumerService) {
@@ -23,29 +31,36 @@ public class DashboardServiceImpl implements DashboardService {
     @Override
     public Optional<DashboardDataset> buildDataset() {
         List<AggregateTradeData> tradeDataList = kafkaConsumerService.poll();
+
         if (tradeDataList.isEmpty()) {
-            return Optional.empty();
+            if (!buyDataCache.isEmpty() && !sellDataCache.isEmpty()) {
+                return Optional.of(returnCachedDataset());
+            } else {
+                return Optional.empty();
+            }
+        } else {
+            tradeDataList.forEach(tradeData -> {
+                DashboardDataset.Data buy = new DashboardDataset.Data();
+                String timeBuy = tradeData.getTime();
+                buy.setTime(timeBuy);
+                buy.setValue(tradeData.getAggregateBuys());
+                buyDataCache.put(timeBuy, buy);
+
+                DashboardDataset.Data sell = new DashboardDataset.Data();
+                String timeSell = tradeData.getTime();
+                sell.setTime(timeSell);
+                sell.setValue(tradeData.getAggregateSales());
+                sellDataCache.put(timeSell, sell);
+            });
+            kafkaConsumerService.commitOffsets();
+            return Optional.of(returnCachedDataset());
         }
-        List<DashboardDataset.Data> sales = new ArrayList<>();
-        List<DashboardDataset.Data> buys = new ArrayList<>();
+    }
 
-        tradeDataList.forEach(tradeData -> {
-            DashboardDataset.Data buy = new DashboardDataset.Data();
-            buy.setTime(tradeData.getTime());
-            buy.setValue(tradeData.getAggregateBuys());
-            buys.add(buy);
-
-            DashboardDataset.Data sell = new DashboardDataset.Data();
-            sell.setTime(tradeData.getTime());
-            sell.setValue(tradeData.getAggregateSales());
-            sales.add(sell);
-        });
-
+    private DashboardDataset returnCachedDataset() {
         DashboardDataset dataset = new DashboardDataset();
-        dataset.setBuy(buys);
-        dataset.setSell(sales);
-
-        kafkaConsumerService.commitOffsets();
-        return Optional.of(dataset);
+        dataset.setBuy(new ArrayList<>(buyDataCache.values()));
+        dataset.setSell(new ArrayList<>(sellDataCache.values()));
+        return dataset;
     }
 }
